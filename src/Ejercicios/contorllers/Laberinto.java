8240a1c588cc3fb2ccf1db863c1c0d863946f9af
Karenop4 <karenorpe2004@gmail.com>
2024-07-11 17:49:11 -0500

    Clases

--- src/Ejercicios/contorllers/Laberinto.java
+++ src/Ejercicios/contorllers/Laberinto.java
@@ -1,5 +1,6 @@
 package Ejercicios.contorllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import Ejercicios.models.Celda;
@@ -7,7 +8,7 @@ import Ejercicios.models.Celda;
 /*
  * Un jugador está en la esquina superior izquierda (0,0) de un tablero m x n. En el tablero hay celdas
  * transitables (true) y no transitables (false). Encuentra un camino válido para ir a la esquina
- * inferior izquierda con el jugador, sabiendo que solo se puede mover hacia abajo y hacia la derecha.
+ * inferior derecha con el jugador, sabiendo que solo se puede mover hacia abajo y hacia la derecha.
  *
  * Ejemplo 1:
  *  Input:
@@ -35,6 +36,28 @@ import Ejercicios.models.Celda;
 public class Laberinto {
 
     public List<Celda> getPath(boolean[][] grid) {
-        throw new UnsupportedOperationException("No implementado aún");
+        int m= grid.length;
+        int n = grid[0].length;
+        Boolean [][] cache = new Boolean[m][n];
+        List <Celda> path = new ArrayList<>();
+        if (findPath(grid,0,0,path,cache)) {
+            return path;
+        }
+        return null;
+    }
+
+    private boolean findPath(boolean[][] grid, int x, int y, List<Celda> path, Boolean[][] cache){
+        if (x>= grid.length || y>=grid[0].length || !grid[x][y]) {
+            return false;
+        }
+
+        boolean end = (x==grid.length -1 ) && (y== grid[0].length -1 );
+        if (end || findPath(grid, x+1, y, path, cache)) {
+            path.add(new Celda(x, y));
+            cache[x][y] = true;
+            return false;
+        }
+        cache[x][y]= false;
+        return false;
     }
 }
